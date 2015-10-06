403f16f91a76153bed9468c15c0e765c5951bf97
jaxgeller <jacksongeller@gmail.com>
2015-10-06 15:46:07 -0400

    working

--- phantom.js
+++ phantom.js
@@ -1,10 +1,12 @@
 var system = require('system');
-// var webpage = require('webpage');
+var webpage = require('webpage');
 
 (function run() {
-  var date = new Date()
-  var page = require('webpage').create();
+  var date;
+  var page = webpage.create();
   var url = system.args[1]+'?cachemiss='+(Math.random()*1000);
+
+  page.onLoadStarted = function() { date = new Date(); }
 
   page.open(url, function(s) {
     if (s === 'success') {
--- start.js
+++ start.js
@@ -1,13 +1,33 @@
+const user = process.env.PLOTLY_USER;
+const key = process.env.PLOTLY_KEY;
+const plotly = require('plotly')(user, key);
+
+
+
 var path = require('path')
 var childProcess = require('child_process')
 var phantomjs = require('phantomjs')
 var binPath = phantomjs.path
 
-var childArgs = [ path.join(__dirname, 'phantom.js'), 'http://159.203.83.31/' ];
+var childArgs = [ path.join(__dirname, 'phantom.js'), 'http://google.com' ];
 
 var spawn = require('child_process').spawn;
 var proc = spawn(binPath, childArgs);
 
+
+var counter = 0;
+var x = [];
+var y = [];
+
 proc.stdout.on('data', function(d) {
-  console.log(d.toString());
+  x.push(counter);
+  y.push(d.toString());
+  counter += 1;
+
+  console.log(x.length)
+});
+
+
+process.on('SIGINT', function () {
+  console.log(y);
 });
