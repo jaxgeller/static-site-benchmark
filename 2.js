e81160ae9f858572169c222b68899b43946139fe
jaxgeller <jacksongeller@gmail.com>
2015-10-06 16:16:21 -0400

    working

--- 2.js
+++
@@ -1,7 +0,0 @@
-var webPage = require('webpage');
-var page = webPage.create();
-var system = require('system');
-
-page.open('http://www.google.com/', function(status) {
-  system.stdout.write(status)
-});
--- start.js
+++ start.js
@@ -2,32 +2,24 @@ const user = process.env.PLOTLY_USER;
 const key = process.env.PLOTLY_KEY;
 const plotly = require('plotly')(user, key);
 
-
-
 var path = require('path')
 var childProcess = require('child_process')
 var phantomjs = require('phantomjs')
 var binPath = phantomjs.path
 
-var childArgs = [ path.join(__dirname, 'phantom.js'), 'http://google.com' ];
+var childArgs = [ path.join(__dirname, 'phantom.js'), 'http://static-site-benchmark.s3-website-us-east-1.amazonaws.com/' ];
 
 var spawn = require('child_process').spawn;
 var proc = spawn(binPath, childArgs);
 
-
 var counter = 0;
-var x = [];
-var y = [];
+
+var fs = require('fs');
+var data = fs.createWriteStream('s3.csv');
+
+data.write('Iteration, Response\n');
 
 proc.stdout.on('data', function(d) {
-  x.push(counter);
-  y.push(d.toString());
+  data.write(counter + ', ' + d.toString() + '\n');
   counter += 1;
-
-  console.log(x.length)
-});
-
-
-process.on('SIGINT', function () {
-  console.log(y);
 });
